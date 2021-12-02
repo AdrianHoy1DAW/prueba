4c8f44ce9bd9b65885677663c6490846858feccb
ahoyoss <ahoyoss@lmde>
2021-12-02 19:19:40 +0100

    Cambios a principal

--- GitEclipse/src/pruebasGit/Prueba1.java
+++ GitEclipse/src/pruebasGit/Prueba1.java
@@ -5,6 +5,8 @@ public class Prueba1 {
 	public static void main(String[] args) {
 		System.out.println("Hola Git");
 		System.out.println("Cambios tras commit");
+		System.out.println("Tercer cambio");
+		System.out.println("Cuarto cambio rama Cambio1");
 	}
 	
 }
