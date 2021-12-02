f1a5f21bed0bc52a5c9d3ec627ad444a4fd5445c
ahoyoss <ahoyoss@lmde>
2021-12-02 19:20:53 +0100

    cambios a cambio1

--- GitEclipse/src/cambiosGit/Cambio1.java
+++ GitEclipse/src/cambiosGit/Cambio1.java
@@ -3,7 +3,7 @@ package cambiosGit;
 public class Cambio1 {
 	
 	public static void main(String[] args) {
-		System.out.println("Pruebas Git,cambio1");
+		System.out.println("Pruebas Git,rama cambio1");
 	}
 	
 }
